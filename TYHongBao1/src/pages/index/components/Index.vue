376f368d81c465c657fe9ca6d35a6adc1ae9622c
John <601473400@qq.com>
2019-06-11 17:20:37 +0800

    init

---
+++ TYHongBao1/src/pages/index/components/Index.vue
@@ -0,0 +1,493 @@
+<template>
+  <div class="index">
+    <div class="top">
+      <div class="logo-box"></div>
+      <div class="title-box"></div>
+      <div class="follow-box">关注"传行"</div>
+    </div>
+    <div class="btm">
+      <div class="btn-box">
+        <div class="btn" @click="clickGet">领红包</div>
+      </div>
+      <div class="ticket-box">
+        <div class="ticket">
+          <div class="ticket-l">
+            <div class="ticket-num">
+              <i class="ticket-icon">￥</i>
+              200
+            </div>
+          </div>
+          <div class="ticket-m">
+            <div class="m-box">
+              <div>优惠券</div>
+              <div>有效期至2019-5-22</div>
+            </div>
+          </div>
+          <div class="ticket-r">立即领取</div>
+        </div>
+      </div>
+      <div class="rule-box">
+        <div class="rule-title">
+          <div class="line line1"></div>
+          <div>活动规则</div>
+          <div class="line line2"></div>
+        </div>
+        <div class="rule-main">
+
+        </div>
+      </div>
+    </div>
+    <div class="btn2">
+      <div class="btn2-box">
+        我的奖品
+      </div>
+    </div>
+    <div class="layer-div" @touchmove.prevent="" v-if="showLayer">
+      <div class="kai" v-if="showKai">
+        <div>
+          <div class="logo-box2">
+            <div class="logo-box2-inner"></div>
+          </div>
+          <div class="kai-font">送了一个红包给你</div>
+          <div class="kai-btn"></div>
+        </div>
+        <div class="tac">
+          <img @click="clickClose" class="cha" src="../assets/cha.png" alt="">
+        </div>
+      </div>
+      <div class="kai2" v-if="showKai2">
+        <div>
+          <div class="kai2-money-box">
+            <div class="kai2-money">
+              <div class="money-box">
+                <span>200</span>
+                <span>元</span>
+              </div>
+            </div>
+          </div>
+          <div class="kai2-mid">
+            <div>恭喜你获得</div>
+            <div>200元红包</div>
+          </div>
+          <div class="kai2-btm">
+            <div class="kai2-btn">知道了</div>
+          </div>
+        </div>
+      </div>
+      <div class="kai3" v-if="showKai3">
+        <div>
+          <img src="../assets/cha-s.png" alt="" class="cha-s" @click="clickClose">
+          <div class="kai3-font1">领取成功！</div>
+          <div class="kai3-font2">可到“我的奖品”页面查看</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { utils, postData } from '../../../common'
+
+export default {
+  data () {
+    return {
+      id: null, // 活动id
+      showLayer: true, // 弹窗
+      showKai: false,
+      showKai2: false,
+      showKai3: true,
+      logoUrl: null, // 酒店logo图片
+      ruleDetail: null, // 活动规则说明
+    }
+  },
+  components: {},
+  methods: {
+    /**
+     * 点击关闭
+     */
+    clickClose () {
+      this.showLayer = false
+    },
+    /**
+     * 点击领取
+     */
+    clickGet () {
+      this.showLayer = true
+    },
+    /**
+     * 点击签到
+     */
+    clickSign () {
+      utils.toast(this, '', 'loading')
+      let posObj = utils.getLocation()
+      // todo 判断经纬度对象是否为空
+      let theData = {
+        ActivityId: this.id,
+        latitude: posObj.lat + '',
+        longitude: posObj.lng + ''
+      }
+      postData('/SignInActivity', theData).then((res) => {
+        console.log(res)
+        utils.toast(this, '', 'clear')
+        this.showLayer = true
+      })
+    }
+  },
+  mounted () {
+
+  },
+  created () {
+    const params = utils.getUrlParams()
+    this.id = params.activityid
+    if (!this.id) {
+      utils.toast(this, '未知活动', 'fail')
+      return
+    }
+    utils.toast(this, '', 'loading')
+    postData('/ActivityInfo', {'ActivityId': this.id}).then((res) => {
+      console.log(res)
+      utils.toast(this, '', 'clear')
+      this.ruleDetail = res.Data.CMA1_CONTENT
+      this.logoUrl = res.Data.CMA1_LOGO_URL
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .index {
+    background-color: #EF4143;
+  }
+
+  .top {
+    background: url("../assets/topbg.png") no-repeat;
+    background-size: 100% 100%;
+    height: 889px;
+    position: relative;
+  }
+
+  .logo-box {
+    position: absolute;
+    top: 27px;
+    left: 0;
+    background: url("../assets/logobg.png") no-repeat;
+    background-size: 100% 100%;
+    width: 324px;
+    height: 76px;
+  }
+
+  .title-box {
+    position: absolute;
+    left: 42px;
+    top: 168px;
+    background: url("../assets/title.png") no-repeat;
+    background-size: 100% 100%;
+    width: 667px;
+    height: 130px;
+  }
+
+  .follow-box {
+    position: absolute;
+    right: 0;
+    top: 34px;
+    color: #FFFEFE;
+    font-size: 28px;
+    width: 196px;
+    height: 72px;
+    background-color: #E74535;
+    border-top-left-radius: 36px;
+    border-bottom-left-radius: 36px;
+    @include defaultFlex;
+  }
+
+  .btm {
+    width: 710px;
+    border-radius: 16px;
+    background-color: #D71820;
+    margin: 0 auto;
+  }
+
+  .btn-box {
+    //    @include defaultFlex;
+    padding: 56px 154px;
+  }
+
+  .btn {
+    width: 402px;
+    height: 96px;
+    background: url("../assets/btnbg.png") no-repeat;
+    background-size: 100% 100%;
+    color: #DA453B;
+    @include defaultFlex;
+    font-weight: bold;
+  }
+
+  .ticket-box {
+    padding: 0 21px;
+  }
+
+  .ticket {
+    height: 224px;
+    width: 100%;
+    background: url("../assets/quan.png") no-repeat;
+    background-size: 100% 100%;
+    margin-bottom: 20px;
+    display: flex;
+    color: #C8151B;
+  }
+
+  .ticket-r {
+    width: 133px;
+    height: 100%;
+    @include borderBox();
+    padding: 30px 57px 30px 44px;
+    font-size: 34px;
+  }
+
+  .ticket-m {
+    width: 276px;
+    height: 100%;
+    @include defaultFlex;
+  }
+
+  .m-box div:nth-child(1) {
+    font-size: 36px;
+    margin-bottom: 20px;
+  }
+
+  .m-box div:nth-child(2) {
+    font-size: 24px;
+  }
+
+  .ticket-l {
+    width: 258px;
+    height: 100%;
+    position: relative;
+  }
+
+  .ticket-num {
+    font-size: 100px;
+    @include center;
+    left: 60%;
+  }
+
+  .ticket-icon {
+    position: absolute;
+    left: -40px;
+    top: -10px;
+    font-size: 46px;
+    font-weight: bold;
+  }
+
+  .rule-box {
+    /*padding: 80px 21px 55px;*/
+    padding: 80px 0 55px;
+  }
+
+  .line {
+    width: 280px;
+    height: 20px;
+  }
+
+  .line2 {
+    background: url("../assets/line.png") no-repeat;
+    background-size: 100% 100%;
+
+  }
+
+  .line1 {
+    background: url("../assets/line.png") no-repeat;
+    transform: rotate(180deg);
+    background-size: 100% 100%;
+
+  }
+
+  .rule-title {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    font-size: 30px;
+    margin-bottom: 60px;
+  }
+
+  .rule-title > div:nth-child(2) {
+    /*padding: 0 30px;*/
+    min-width: 120px;
+    padding: 0 20px;
+    white-space: nowrap;
+  }
+  .rule-main {
+    font-size: 28px;
+    color: #fff;
+    padding: 0 21px;
+
+    /deep/ *{
+      padding:0;
+      margin:0;
+      font-size: 28px;
+      color: #fff;
+    }
+  }
+  .btn2 {
+    padding: 60px 0 120px;
+    display: flex;
+    justify-content: center;
+  }
+  .btn2-box {
+    width: 402px;
+    height: 90px;
+    background: url("../assets/btnbg.png") no-repeat;
+    background-size: 100% 100%;
+    @include defaultFlex;
+    font-size: 36px;
+    color: #DA453B;
+    font-weight: bold;
+  }
+  .layer-div {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    background: rgba(0,0,0,0.3);
+  }
+  .kai, .kai2, .kai3 {
+    @include center;
+  }
+  .kai2 {
+    width: 100%;
+    @include defaultFlex;
+  }
+  .kai > div:nth-child(1) {
+    position: relative;
+    margin-bottom: 42px;
+    width: 552px;
+    height: 740px;
+    background: url("../assets/kai.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .kai2 > div:nth-child(1) {
+    position: relative;
+    width: 714px;
+    height: 900px;
+    background: url("../assets/kai2.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .kai3 > div:nth-child(1) {
+    position: relative;
+    width: 585px;
+    height: 572px;
+    background: url("../assets/kai3.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .kai-btn {
+    position: absolute;
+    width: 163px;
+    height: 163px;
+    left: 195px;
+    top: 435px;
+  }
+  .kai .img1 {
+    margin-bottom: 42px;
+    width: 552px;
+  }
+  .kai .cha {
+    width: 72px;
+  }
+  .tac {
+    text-align: center;
+  }
+  .logo-box2 {
+    position: relative;
+    top: 129px;
+    display: flex;
+    justify-content: center;
+    img {
+      max-height: 100%;
+    }
+  }
+  .logo-box2-inner {
+    width: 391px;
+    height: 72px;
+    background: url("../assets/logoBg2.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .kai-font {
+    font-size: 36px;
+    color: #fff;
+    margin-top: 30px;
+    text-align: center;
+    position: relative;
+    top: 130px;
+  }
+  .kai2-money-box {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 50px;
+  }
+  .kai2-money {
+    width: 305px;
+    height: 268px;
+    @include defaultFlex;
+    color: #B15A23;
+    margin-top: 80px;
+  }
+  .money-box {
+    position: relative;
+    span:nth-child(1) {
+      font-size: 80px;
+    }
+    span:nth-child(2) {
+      font-size: 40px;
+    }
+  }
+  .kai2-mid {
+    color: #fff;
+    text-align: center;
+    div:nth-child(1) {
+      font-size: 36px;
+      margin-bottom: 32px;
+    }
+    div:nth-child(2) {
+      font-size: 48px;
+    }
+  }
+  .kai2-btm {
+    margin-top: 90px;
+    @include defaultFlex;
+    .kai2-btn {
+      width: 402px;
+      height: 96px;
+      background: url("../assets/btnbg.png") no-repeat;
+      background-size: 100% 100%;
+      @include defaultFlex;
+      color: #DC5A51;
+      font-size: 36px;
+      font-weight: bold;
+    }
+  }
+  .kai3 {
+    text-align: center;
+  }
+  .cha-s {
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    width: 30px;
+    z-index: 10;
+  }
+  .kai3-font1 {
+    color: #fff;
+    font-size: 36px;
+    position: relative;
+    top: 40px;
+  }
+  .kai3-font2 {
+    color: #fff;
+    font-size: 28px;
+    position: relative;
+    top: 53px;
+  }
+</style>
---
+++ TYHongBao1/src/pages/myPrize/components/MyPrize.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="album">
+    <div ref="header" class="header">
+      <van-nav-bar
+        title="我的奖品"
+        left-text=""
+        right-text=""
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right=""
+      />
+    </div>
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh" id="img-box" class="img-box" ref="img-box">
+      <!--<div class="img-box" ref="img-box">-->
+        <van-list
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
+          <ul ref="img-ul" class="img-ul">
+            <li class="img-li" ref="img-li" v-for="(item,index) in list" :key="index">
+              <div class="li-box">
+                <div class="li-line1">现金红包</div>
+                <div class="li-line2">
+                  888
+                  <i>元</i>
+                </div>
+                <div class="li-line3">有效期至2019-5-22</div>
+              </div>
+            </li>
+          </ul>
+        </van-list>
+      <!--</div>-->
+
+    </van-pull-refresh>
+  </div>
+</template>
+
+<script>
+import { utils, postData } from '../../../common'
+
+export default {
+  data () {
+    return {
+      id: null, // 活动id
+      list: [],
+      loading: false,
+      finished: false,
+      isLoading: false
+    }
+  },
+  components: {},
+  mounted () {
+//    setTimeout(() => {
+//      this.setImgBoxHeight()
+//    }, 300)
+    this.setImgBoxHeight2()
+  },
+  created () {
+    const params = utils.getUrlParams()
+    this.id = params.activityid
+    if (!this.id) {
+      utils.toast(this, '未知活动', 'fail')
+      return
+    }
+    utils.toast(this, '', 'loading')
+    postData('/MyActivityPrizes', {ActivityId: this.id}).then((res) => {
+      console.log(res)
+      utils.toast(this, '', 'clear')
+      this.list = res.Data.IList
+    })
+  },
+  methods: {
+    /**
+     * @method 设置滚动容器的高度
+     */
+    setImgBoxHeight2 () {
+      let windowHeight = document.body.clientHeight;
+      let headerHeight = this.$refs.header.offsetHeight
+      console.log(`windowHeight: ${windowHeight}, headerHeight: ${headerHeight}`)
+//      this.$refs['img-box'].style.height = (windowHeight - headerHeight) + 'px'
+      let imgBox = document.getElementById('img-box')
+      imgBox.style.height = (windowHeight - headerHeight) + 'px'
+    },
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    },
+    onClickLeft () {
+      window.history.back()
+    },
+    onRefresh () {
+      setTimeout(() => {
+        this.$toast('刷新成功')
+        this.isLoading = false
+      }, 500)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .album {
+    width: 100%;
+    background-color: #F5F0F0;
+  }
+
+  .header {
+    position: relative;
+  }
+
+  .uploader-box {
+    position: absolute;
+    right: 0.75rem;
+    top: 12px;
+    z-index: 1;
+  }
+
+  .img-box {
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch; /* 解决ios滑动不流畅问题 */
+  }
+
+  .img-ul {
+    /*display: flex;*/
+    overflow: hidden;
+    li {
+      width: 100%;
+      @include borderBox();
+      padding: 18px 25px;
+    }
+  }
+  .li-box {
+    height: 208px;
+    width: 100%;
+    background: url("../assets/hognbao.png") no-repeat;
+    background-size: 100% 100%;
+    @include borderBox();
+    color: #fff;
+    position: relative;
+  }
+  .li-line1 {
+    font-size: 46px;
+    position: absolute;
+    top: 50px;
+    left: 70px;
+  }
+  .li-line2 {
+    font-size: 46px;
+    font-weight: bold;
+    position: absolute;
+    top: 120px;
+    left: 70px;
+    i {
+      font-size: 26px;
+    }
+  }
+  .li-line3 {
+    font-size: 26px;
+    position: absolute;
+    right: 30px;
+    bottom: 30px;
+  }
+</style>
