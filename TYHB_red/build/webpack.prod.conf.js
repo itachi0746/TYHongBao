8fe2e56c8d18f648bd23eb94493f00939565021a
John <601473400@qq.com>
2019-10-31 09:47:19 +0800

    添加总列表页

--- TYHB_red/build/webpack.prod.conf.js
+++ TYHB_red/build/webpack.prod.conf.js
@@ -121,7 +121,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'common-api',
-      chunks: ['admin', 'index','invitation','album', 'couponDetail'],
+      chunks: ['admin', 'index','invitation','album', 'couponDetail','activity'],
       minChunks: Infinity
     }),
     // extract webpack runtime and module manifest to its own file in order to
